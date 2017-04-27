292939d60733ca2bd533345a9ae6a7662ed72bf9
Sethmh82 <seth@statisticbrain.com>
2017-04-27 03:55:18 -0700

    added https

--- giphy.js
+++ giphy.js
@@ -5,7 +5,7 @@ function displaygiphyInfo() {
 
   var theGiphy = $(this).attr("data-name");
 
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + theGiphy + "&api_key=dc6zaTOxFJmzC";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + theGiphy + "&api_key=dc6zaTOxFJmzC";
   $.ajax({
     url: queryURL,
     method: "GET"
